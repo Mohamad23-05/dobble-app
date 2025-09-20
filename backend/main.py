dabc9bf008423702fab69292b5b1329fdb919edd
Mohamad Kadabashi <s85438@htw-dresden.de>
2025-09-20 19:26:27 +0200

    fix CROS

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,23 @@
 import os
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse, StreamingResponse
-from pydantic import BaseModel, Field
-from io import BytesIO
-
 from backend.routers import dobble
 
 app = FastAPI(title="Dobble API")
-FRONTEND_URL = os.getenv("FRONTEND_URL")  # set to https://dobble-app.onrender.com
+
+FRONTEND_URL = os.getenv("FRONTEND_URL", "https://dobble-app.onrender.com")
+ALLOWED_ORIGINS = [
+    FRONTEND_URL,  # production
+    "http://localhost:5173",  # local dev
+    "http://127.0.0.1:5173",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL, "http://localhost:5173", "http://127.0.0.1:5173"],
-    allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+    allow_origins=ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(dobble.router)
