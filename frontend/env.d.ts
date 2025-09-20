d71dbc4a89ac4b5b1d1f45a2b052a320914be087
Mohamad Kadabashi <s85438@htw-dresden.de>
2025-09-20 17:17:00 +0200

    update branding: change favicon to dobbleLogo, update app title, and add Vue type declarations to env file

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,1 +1,6 @@
-/// <reference types="vite/client" />
+// src/env.d.ts
+declare module '*.vue' {
+  import type {DefineComponent} from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
